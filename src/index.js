f6cc4523ce4b01460cd4b43a7516c5586eb022e2
Manish Kumar Rajak <52307777+visionxStudio@users.noreply.github.com>
2021-02-22 23:25:54 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,15 +14,5 @@ ReactDOM.render(
   </FirebaseContext.Provider>,
   document.getElementById('root'),
 );
- // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-
-
-/*
-
-Doing it this way, we can be assured that Firebase is only instantiated once and that it is injected via React's Context API to React's component tree. Now, every component that is interested in using Firebase has access to the Firebase instance with a FirebaseContext.Consumer component. Even though you will see it first-hand later for this application, the following code snippet shows how it would work:
-
-*/
